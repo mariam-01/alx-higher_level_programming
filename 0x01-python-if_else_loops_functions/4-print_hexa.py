771c08e3d2eb2d03bd45842329843c079262a846
mariam-01 <mariam.larhlid2001@gmail.com>
2023-07-11 13:48:41 -0700

    task3

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(99):
-    print(f"{i} = {hex(i)}")
-    #print("{} = {}".format(10, hex(10)))
+    #print(f"{i} = {hex(i)}")
+    print("{} = {}".format(10, hex(10)))
     #print("{} = 0x{:x}".format(i, i))
