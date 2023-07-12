b91cbb5daf2a8293dc21dca86631d2bd1d8b3fd6
mariam-01 <mariam.larhlid2001@gmail.com>
2023-07-12 08:42:23 -0700

    second deadline

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     copy = ''
-    for c in str:
-        if c == str[n]:
-            copy = copy
-        else:
-            copy += c
-    return copy    
-
+    if (n < len(str) and n > 0):
+        for c in str:
+            if c != str[n]:
+                copy += c
+    else:
+        return str
+    return copy
