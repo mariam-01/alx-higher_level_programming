c9264412810d8cd94f1f04a98a66ec77038b90ea
mariam-01 <mariam.larhlid2001@gmail.com>
2023-07-11 17:37:32 -0700

    task

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-for i in range(ord('Z'), ord('A') - 1, -1):
-    print("{:c}{:c}".format(i + 32, i), end='')
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    if i == 0:
+        i = 32  
+    else:
+        i = 0
