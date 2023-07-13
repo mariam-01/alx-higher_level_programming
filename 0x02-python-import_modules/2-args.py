1f50bb3a474d59b3ed50865263148280c684853f
mariam-01 <mariam.larhlid2001@gmail.com>
2023-07-13 14:05:12 -0700

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
         print("1 argument:")
         
     else:
-        print("{} arguments".format(n))
+        print("{} arguments:".format(n))
     for i in range(1, n + 1):
         print("{}: {}".format(i, a[i]))
