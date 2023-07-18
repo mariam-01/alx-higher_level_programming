be98dd6deebac13ae2ee785f1e73fde618de9975
mariam-01 <mariam.larhlid2001@gmail.com>
2023-07-18 11:15:14 -0700

    task9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) < 0:
+        return None
+    else:
+        max = 0
+        for n in my_list:
+            if max < n:
+                max = n
+        return max
